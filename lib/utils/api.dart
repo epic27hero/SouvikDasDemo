d2ee0a7d38591277b3a44433161d5692d1a3d790
SOUVIK VERSATILE <souvikversatile@SOUVIKs-MacBook-Pro.local>
2020-06-17 13:25:45 +0530

    First Commit

---
+++ lib/utils/api.dart
@@ -0,0 +1,5 @@
+class Api{
+  static const api='https://api.github.com';
+//  static const token='be08e5aae122bb17be4f5e950fa0090d9486fc34';
+  static const token ='93d6e4601b7b8361d9f339754ae37f7f9d74bf70';
+}
